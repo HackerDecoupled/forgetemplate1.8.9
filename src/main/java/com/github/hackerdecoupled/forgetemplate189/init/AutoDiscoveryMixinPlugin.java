2f4f30c5956a4e539ea516ee2c65edceed131357
GitHub Action <action@github.com>
2024-02-17 01:31:15 +0000

    Initialize template repository proper
    Set package name to com.github.hackerdecoupled.forgetemplate189
    Set project name to forgetemplate1.8.9
    Set modid to forgetemplate

--- src/main/java/com/github/hackerdecoupled/forgetemplate189/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/hackerdecoupled/forgetemplate189/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.hackerdecoupled.forgetemplate189.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
